39b26602e463b5d15e15175d607ebf9f033cc122
Ismail Masmoudi <155138711+ismailmasmoudi@users.noreply.github.com>
2024-12-12 15:53:38 +0100

    add introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { IntroductionComponent } from "./main-content/introduction/introduction.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, IntroductionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
