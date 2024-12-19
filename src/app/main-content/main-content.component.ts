c38e36a85ea0fdebeb5f1ebd80999cb74affe529
Ismail Masmoudi <155138711+ismailmasmoudi@users.noreply.github.com>
2024-12-19 12:40:41 +0100

    add porfolio area

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { IntroductionComponent } from "./introduction/introduction.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AboutMeComponent, IntroductionComponent, MySkillsComponent],
+  imports: [AboutMeComponent, IntroductionComponent, MySkillsComponent, PortfolioComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
