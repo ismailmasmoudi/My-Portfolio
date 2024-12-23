3671a87ca06014aa3eedc6d4fac03d739d55ca4b
Ismail Masmoudi <155138711+ismailmasmoudi@users.noreply.github.com>
2024-12-23 07:58:40 +0100

    add contact

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { AboutMeComponent } from "./about-me/about-me.component";
 import { IntroductionComponent } from "./introduction/introduction.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AboutMeComponent, IntroductionComponent, MySkillsComponent, PortfolioComponent],
+  imports: [AboutMeComponent, IntroductionComponent, MySkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
