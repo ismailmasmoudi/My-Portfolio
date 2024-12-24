258e7b5d223d546146a2d4c4b2e1a825d314e526
Ismail Masmoudi <155138711+ismailmasmoudi@users.noreply.github.com>
2024-12-24 20:36:12 +0100

    add form validtion in contact component

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,16 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit ,inject,  ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  @ViewChild('contactForm') contactForm!: NgForm;
 
+  // ... your other code
+
+  validateForm() {
+    if(this.contactForm) {
+     Object.keys(this.contactForm.controls).forEach(key => {
+      this.contactForm.controls[key].markAsTouched();
+    });
+  }
+    // This function can also be used to check the form's overall validity
+    // if you need to enable/disable buttons or perform other actions.
+  }
+
+  ngOnInit() {
+    // You likely no longer need this as validation will occur on input
+   // this.contactForm.controls[controlName].markAsTouched();       }
+
+
+  // ... your other code
+}
   http= inject(HttpClient);
 
   contactData = {
@@ -32,6 +53,14 @@ export class ContactComponent {
     },
   };
 
+  // ngOnInit() {
+  //   for (const controlName in this.contactForm.controls) {
+  //     if (this.contactForm.controls.hasOwnProperty(controlName)) {
+  //       this.contactForm.controls[controlName].markAsTouched();
+  //     }
+  //   }
+  // }
+
   onSubmit(ngForm: NgForm) {
     if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
       this.http.post(this.post.endPoint, this.post.body(this.contactData))
@@ -50,5 +79,12 @@ export class ContactComponent {
       ngForm.resetForm();
     }
   }
-
-}
+  // onFocus(fieldName: string) {
+  //   // No specific action needed here, but you can add if required.
+  //  }
+   
+  //  onBlur(fieldName: string) {
+  //   // No specific action needed here, but you can add if required. For instance resetting border if desired
+  //  }
+   }
+   
