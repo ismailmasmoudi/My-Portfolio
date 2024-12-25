a043014e10952b1e6c45102b44fec66b73967077
Ismail Masmoudi <155138711+ismailmasmoudi@users.noreply.github.com>
2024-12-26 00:00:05 +0100

    fic contact funktion

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,38 +1,59 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit ,inject,  ViewChild } from '@angular/core';
+import { Component, OnInit, inject, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent implements OnInit {
+  imageUrl = 'img/Go-up-button.svg';
+  privacyAgreementChecked = false;
+  checkboxHover = false;
+  checkboxImageUrl = 'img/Check-button.svg';
+
   @ViewChild('contactForm') contactForm!: NgForm;
 
-  // ... your other code
+  getCheckboxImage() {  // Modified to handle hover
+    if (this.privacyAgreementChecked) {
+      return this.checkboxHover ? 'img/Check-button-checked-hover.svg' : 'img/Check-button-checked.svg';
+    } else {
+      return this.checkboxHover ? 'img/Check-button-hover.svg' : 'img/Check-button.svg';
+    }
+  }
 
   validateForm() {
-    if(this.contactForm) {
-     Object.keys(this.contactForm.controls).forEach(key => {
-      this.contactForm.controls[key].markAsTouched();
-    });
-  }
-    // This function can also be used to check the form's overall validity
-    // if you need to enable/disable buttons or perform other actions.
+    if (this.contactForm) {
+      Object.keys(this.contactForm.controls).forEach(key => {
+        this.contactForm.controls[key].markAsTouched();
+      });
+    }
+
   }
 
   ngOnInit() {
-    // You likely no longer need this as validation will occur on input
-   // this.contactForm.controls[controlName].markAsTouched();       }
+    this.updateCheckboxImage(); // Set initial image
+  }
+
+  togglePrivacyAgreement() {
+    this.privacyAgreementChecked = !this.privacyAgreementChecked;
+    this.updateCheckboxImage(); // Update image after click
+  }
 
 
-  // ... your other code
-}
-  http= inject(HttpClient);
+
+  updateCheckboxImage() {  // Renamed for clarity
+    if (this.privacyAgreementChecked) {
+      this.checkboxImageUrl = this.checkboxHover ? './img/Check-button-checked-hover.svg' : './img/Check-button-checked.svg';
+    } else {
+      this.checkboxImageUrl = this.checkboxHover ? './img/Check-button-hover.svg' : './img/Check-button.svg';
+    }
+  }
+
+  http = inject(HttpClient);
 
   contactData = {
     name: '',
@@ -53,38 +74,41 @@ export class ContactComponent implements OnInit {
     },
   };
 
-  // ngOnInit() {
-  //   for (const controlName in this.contactForm.controls) {
-  //     if (this.contactForm.controls.hasOwnProperty(controlName)) {
-  //       this.contactForm.controls[controlName].markAsTouched();
-  //     }
-  //   }
-  // }
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
   onSubmit(ngForm: NgForm) {
-    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+    if (!this.privacyAgreementChecked) {
+
+      this.validateForm();
+      return;
+    }
+
+    if (ngForm !== this.contactForm) return;
+    if (ngForm.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData), this.post.options) // Add options
         .subscribe({
           next: (response) => {
-            // du kannst hier was hinzufügen
+            console.log("Response from server:", response);
             ngForm.resetForm();
+            this.privacyAgreementChecked = false;
           },
           error: (error) => {
             console.error(error);
+            // Display error messages
+            alert("Failed to send email. Please try again.");
           },
-          complete: () => console.info('send post complete'),
         });
-    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-
+    } else if (ngForm.valid && this.mailTest) {
+      console.log('Test mode: Form submitted', this.contactData);
       ngForm.resetForm();
+      this.privacyAgreementChecked = false;
+    } else {
+      this.validateForm();
+
     }
   }
-  // onFocus(fieldName: string) {
-  //   // No specific action needed here, but you can add if required.
-  //  }
-   
-  //  onBlur(fieldName: string) {
-  //   // No specific action needed here, but you can add if required. For instance resetting border if desired
-  //  }
-   }
-   
+}
+
