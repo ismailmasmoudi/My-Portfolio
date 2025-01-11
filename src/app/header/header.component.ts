21ad6ab3abde32cdc221219eef8357a9ebb58f93
Ismail Masmoudi <155138711+ismailmasmoudi@users.noreply.github.com>
2025-01-11 23:04:50 +0100

    add Burger-Menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,39 +1,53 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject ,ChangeDetectorRef} from '@angular/core';
 import {  CommonModule } from '@angular/common';
 import { TranslationService } from '../services/translation.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule ], // Import here
+  imports: [CommonModule ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
   translationService = inject(TranslationService);
   isGerman$ = this.translationService.isGerman$;
-
+  showBurgerMenu: boolean = false;
   translationIconSrc: string | undefined;
-
-  constructor() { // Use the observable in the constructor
+   burgerMenuIconSrc: string = 'img/menu.svg';
+   changeDetectorRef = inject(ChangeDetectorRef);
+  constructor() { 
      this.isGerman$.subscribe(isGerman => {
        this.translationIconSrc = isGerman ? 'img/translate/german.png' : 'img/translate/english.png';
      });
   }
 
 
-  // In header.component.ts:
   toggleTranslation(): void {
     this.translationService.toggleTranslation();
-
-    // No need to manually change the image source here. The isGerman$ observable handles it.
-
     const toggleElement = document.querySelector('.translation-toggle');
     if (toggleElement) {
       toggleElement.classList.toggle('active');
     }
   }
 
+
+  toggleBurgerMenu() {
+    this.showBurgerMenu = !this.showBurgerMenu;
+    this.burgerMenuIconSrc = this.showBurgerMenu ? 'img/close.svg' : 'img/menu.svg';
+    this.changeDetectorRef.detectChanges();
+      const burgerIcon = document.querySelector('.burger-menu-icon');
+      if (burgerIcon) {
+        burgerIcon.classList.toggle('open');
+      }
+  }
+
+
+  closeBurgerMenu() {
+    this.showBurgerMenu = false;
+    this.burgerMenuIconSrc = 'img/menu.svg';
+    this.changeDetectorRef.detectChanges();
+  }
 
 
   scrollToSection(sectionId: string): void {
