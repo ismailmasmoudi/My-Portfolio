26c03330bf9c2bdaa9161b7b0d645f6214ebbc12
Ismail Masmoudi <155138711+ismailmasmoudi@users.noreply.github.com>
2025-01-21 16:25:45 +0100

    final edition

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -16,7 +16,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'contact@ismail.masmoudi.com';  
+            $recipient = 'ismail.masmoudi7@gmail.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
